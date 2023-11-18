2eba54887059c07b7bb5f7ae786a677b18664cd4
elranas <mauriciosantana962@yahoo.com>
2023-11-18 16:41:09 -0300

    gif added on landing page

--- client/src/Views/Landing/Landing.jsx
+++ client/src/Views/Landing/Landing.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import styles from "../Landing/Landing.module.css"
+import gifImage from "../Landing/Rotating_globe.gif"
+
 
 const Landing = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Countries SPA</h1>
+
+      <img src={gifImage} alt="globeGif" />
+
       <Link to="/home">
         <button className={styles.button}>
           Ingresar
